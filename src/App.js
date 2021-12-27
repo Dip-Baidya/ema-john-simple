d6d184e218301a76a173ff40384fe5fed4869d25
Dip-Baidya <dipbaidya.cse@gmail.com>
2021-12-27 19:52:40 +0600

    add Event-Handler and also understant pass perameter in a handler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './componenets/Header/Header';
+import Shop from './componenets/Shop/Shop';
 
 function App() {
   return (
     <div>
+      <Header></Header>
+      <Shop></Shop>
       
     </div>
   );
