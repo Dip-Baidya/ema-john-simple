50f3a5292ba69933082a44f5a5bc000d304aac03
Dip-Baidya <dipbaidya.cse@gmail.com>
2022-01-03 18:11:21 +0600

    Add React Router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,40 @@ import React from 'react';
 import './App.css';
 import Header from './componenets/Header/Header';
 import Shop from './componenets/Shop/Shop';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Inventory from './componenets/Inventory/Inventory';
+import NotFound from './componenets/NotFound/NotFound';
+import ProductDetail from './componenets/ProductDetail/ProductDetail';
+//import {Switch} from 'react-router'
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
-      
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path='/product/:productKey'>
+            <ProductDetail></ProductDetail>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/componenets/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h4>Sorry, page not found</h4>
+            <h3>404</h3>
+        </div>
+    );
+};
+
+export default NotFound;
