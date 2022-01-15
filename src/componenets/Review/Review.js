44c741f94fc0d077da234a4dbf8d7188819f56fe
Dip-Baidya <dipbaidya.cse@gmail.com>
2022-01-15 20:35:55 +0600

    Adjust Review Section Also Cart Section

--- src/componenets/Review/Review.js
+++ src/componenets/Review/Review.js
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
-import { getDatabaseCart } from '../../utilities/databaseManager';
+import { getDatabaseCart, removeFromDatabaseCart } from '../../utilities/databaseManager';
+import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
     const [cart, setCart] = useState([]);
 
+    const removeProduct = (productKey) => {
+        //console.log('Clicked Remove', productKey);
+        const newCart = cart.filter(pd => pd.key !== productKey);
+        setCart(newCart);
+        removeFromDatabaseCart(productKey);
+    }
+
     useEffect(() => {
         //cart
         const savedCart = getDatabaseCart();
-        const productKeys = Object.keys(savedCart); // Sudhu Kye Guloke newa
-        console.log(productKeys);
+        const productKeys = Object.keys(savedCart); // Sudhu KEY Guloke newa
+        //console.log(productKeys);
         const cartProducts = productKeys.map(key => {
             const product = fakeData.find(pd => pd.key === key);
             product.quantity = savedCart[key];
@@ -19,13 +27,19 @@ const Review = () => {
         setCart(cartProducts);
     }, [])
     return (
-        <div>
-            <h1>Cart Items:{cart.length}</h1>
-            {
-                cart.map(pd => <ReviewItem
-                    key={pd.key}
-                    product={pd}></ReviewItem>)
-            }
+        <div className="twin-container">
+            <div className="product-container">
+                {/* <h1>Cart Items:{cart.length}</h1> */}
+                {
+                    cart.map(pd => <ReviewItem
+                        key={pd.key}
+                        removeProduct={removeProduct}
+                        product={pd}></ReviewItem>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
